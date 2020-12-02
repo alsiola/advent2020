f2d0ff4ad3a2aef6325f3a9ea4732d7d050d8561
Alex Young <alex.young@hive.hr>
2020-12-02 11:09:53 +0000

    Day 2 Part 2

---
+++ src/day2/part2.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const data = readFileSync(join(__dirname, "input.txt"))
+    .toString()
+    .split("\n")
+    .map(s => s.split(" ") as [string, string, string]);
+
+const valid = data.reduce((count, [range, charS, password]) => {
+    const [pos1, pos2] = range.split("-").map(Number);
+    const char = charS[0];
+
+    const charAtPos1 = password[pos1 - 1];
+    const charAtPos2 = password[pos2 - 1];
+
+    if (charAtPos1 === charAtPos2) {
+        return count;
+    }
+
+    if (charAtPos1 === char || charAtPos2 === char) {
+        return ++count;
+    }
+
+    return count;
+}, 0);
+
+console.log(valid);
