bbc65b0633f0c7fb0d4e682ba3ab740808ccdcf3
Alex Young <alex.young@hive.hr>
2020-12-02 10:55:49 +0000

    Day 1 Part 2

---
+++ src/day1/part2.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const data = readFileSync(join(__dirname, "input.txt")).toString().split("\n").map(Number);
+
+const vals = new Set(data);
+
+for (const val of vals) {
+    for (const val2 of vals) {
+        if (vals.has(2020 - val - val2)) {
+            console.log(val * (2020 - val - val2) * val2);
+            process.exit(0)
+        }
+    }
+}
+console.error("No match!")
+process.exit(1)
