36df45773e4e2a866d95b630803a44df6f645a49
Alex Young <alex.young@hive.hr>
2020-12-10 21:56:36 +0000

    Day 10 Part 2

---
+++ src/day10/part2.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const jolts = readFileSync(join(__dirname, "input.txt"))
+    .toString()
+    .split("\n")
+    .map((v) => parseInt(v))
+    .sort((a, b) => a - b);
+
+jolts.unshift(0);
+
+const getNext = (jolt: number): number[] =>
+    jolts.filter((j) => {
+        const d = j - jolt;
+        return d > 0 && d <= 3;
+    });
+
+const nexts = new Map<number, number[]>();
+const journeys = new Map<number, number>();
+
+jolts.forEach((j) => {
+    nexts.set(j, getNext(j));
+});
+
+jolts.reverse().forEach((j) => {
+    const jNexts = nexts.get(j)!;
+
+    if (jNexts.length < 1) {
+        journeys.set(j, 1);
+        return;
+    }
+
+    journeys.set(
+        j,
+        jNexts.map((jn) => journeys.get(jn)!).reduce((a, b) => a + b, 0)
+    );
+});
+
+console.log(journeys.get(0));
